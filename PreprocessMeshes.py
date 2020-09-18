0b0dab19523fb53af601f4fa605f12942b67155a
Kayleigh Schoorl <kayleighschoorl@hotmail.com>
2020-09-18 21:33:14 +0200

    Add smoothing option to read_meshes, excel writing fix

--- PreprocessMeshes.py
+++ PreprocessMeshes.py
@@ -4,6 +4,8 @@ import os, stat
 from pathlib import Path
 import meshlabxml as mlx
 
+meshlabserver_path = '/Applications/meshlab.app/Contents/MacOS'
+os.environ['PATH'] = meshlabserver_path + os.pathsep + os.environ['PATH']
 
 def has_hidden_attribute(filepath):
     return bool(os.stat(filepath).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)
@@ -30,14 +32,21 @@ for mesh_class in os.listdir(db_path):
             faces = len(mesh.faces)
             if faces > 20000:
                 converted_mesh_path = os.path.join(curr_directory, "converted", name + ".ply")
-                script = mlx.FilterScript(file_in=converted_mesh_path, file_out=os.path.join("simplified", name + ".ply"), ml_version='2016.12')
+                # script = mlx.FilterScript(file_in=converted_mesh_path, file_out=os.path.join("simplified", name + ".ply"), ml_version='2016.12')
 
-                mlx.remesh.simplify(script, texture=False, faces=20000,
-                    target_perc=0.0, quality_thr=1.0, preserve_boundary=True,
-                    boundary_weight=1.0, preserve_normal=True,
-                    optimal_placement=True, planar_quadric=True,
-                    selected=False, extra_tex_coord_weight=1.0)
+                # mlx.remesh.simplify(script, texture=False, faces=20000,
+                #     target_perc=0.0, quality_thr=1.0, preserve_boundary=True,
+                #     boundary_weight=1.0, preserve_normal=True,
+                #     optimal_placement=True, planar_quadric=True,
+                #     selected=False, extra_tex_coord_weight=1.0)
                 
-                script.run_script()
+                # script.run_script()
+
+            elif faces < 20000:
+                new_vertices, new_faces = trimesh.remesh.subdivide(mesh.vertices, mesh.faces)
+
+                print("mesh "+str(filename))
+                print(len(mesh.faces))
+                print(len(new_faces))
 
 
--- read_meshes.py
+++ read_meshes.py
@@ -4,13 +4,14 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-M", "--mesh", help="input mesh")
+parser.add_argument("-S", "--smooth", help="show smooth mesh", action="store_true")
 
 args = parser.parse_args()
 
 if args.mesh:
     print("Opening mesh...")
     mesh = trimesh.load(args.mesh)
-    mesh.show()
+    mesh.show(smooth=args.smooth)
 else:
     print("Please input a mesh.")
     print("Usage: -M path/to/mesh")
