6444ca1a834f7ee6e032b746f1802e114bf747dc
Kayleigh Schoorl <kayleighschoorl@hotmail.com>
2020-09-23 23:04:27 +0200

    add scaling

--- normalize.py
+++ normalize.py
@@ -4,7 +4,145 @@ import os, stat
 from pathlib import Path
 import meshlabxml as mlx
 
-meshlabserver_path = 'C:\\Program Files\\VCG\\MeshLab'
+
+# Code for writing a MeshLab script for scaling by 3DLIRIOUS
+# https://github.com/3DLIRIOUS/MeshLabXML
+def make_list(var, num_terms=1):
+    """ Make a variable a list if it is not already
+    If variable is not a list it will make it a list of the correct length with
+    all terms identical.
+    """
+    if not isinstance(var, list):
+        if isinstance(var, tuple):
+            var = list(var)
+        else:
+            var = [var]
+    #if len(var) == 1:
+            for _ in range(1, num_terms):
+                var.append(var[0])
+    return var
+
+
+def write_filter(script, filter_xml):
+    """ Write filter to FilterScript object or filename
+    Args:
+        script (FilterScript object or filename str): the FilterScript object
+            or script filename to write the filter to.
+        filter_xml (str): the xml filter string
+    """
+    if isinstance(script, mlx.FilterScript):
+        script.filters.append(filter_xml)
+    elif isinstance(script, str):
+        script_file = open(script, 'a')
+        script_file.write(filter_xml)
+        script_file.close()
+    else:
+        print(filter_xml)
+    return None
+
+
+def scale_fixed(script, value=1.0, uniform=True, center_pt='origin',
+           custom_center_pt=None, unit=False, freeze=True, all_layers=False):
+    """
+    Args:
+        script: the FilterScript object or script filename to write
+            the filter to.
+        value (float): Scaling along the X axis.
+        uniform (bool): If selected an uniform scaling (the same for all the',
+        ' three axis) is applied (the X axis value is used).
+        center_pt (str): Choose a method.
+        custom_center_pt (point): This scaling center is used only if the',
+        ' _custom point_ option is chosen.
+        unit (bool): If selected, the object is scaled to a box whose',
+        ' sides are at most 1 unit length.
+        freeze (bool): The transformation is explicitly applied and the',
+        ' vertex coords are actually changed.
+        all_layers (bool): The transformation is explicitly applied to all the',
+        ' mesh and raster layers in the project.
+    """
+    """# Convert value to list if it isn't already
+    if not isinstance(value, list):
+        value = list(value)
+    # If a single value was supplied use it for all 3 axes
+    if len(value) == 1:
+        value = [value[0], value[0], value[0]]"""
+    value = make_list(value, 3)
+    # Convert center point name into number
+    if center_pt.lower() == 'origin':
+        center_pt_num = 0
+    elif center_pt.lower() == 'barycenter':
+        center_pt_num = 1
+    else:  # custom axis
+        center_pt_num = 2
+        if custom_center_pt is None:
+            print('WARNING: a custom center point was selected, however',
+                  '"custom_center_pt" was not provided. Using default',
+                  '(origin).')
+            custom_center_pt = [0.0, 0.0, 0.0]
+    filter_xml = ''.join([
+        '  <filter name="Transform: Scale, Normalize">\n',
+        '    <Param name="axisX" ',
+        'value="%s" ' % value[0],
+        'description="X Axis" ',
+        'type="RichFloat" ',
+        '/>\n',
+        '    <Param name="axisY" ',
+        'value="%s" ' % value[1],
+        'description="Y Axis" ',
+        'type="RichFloat" ',
+        '/>\n',
+        '    <Param name="axisZ" ',
+        'value="%s" ' % value[2],
+        'description="Z Axis" ',
+        'type="RichFloat" ',
+        '/>\n',
+        '    <Param name="uniformFlag" ',
+        'value="%s" ' % str(uniform).lower(),
+        'description="Uniform Scaling" ',
+        'type="RichBool" ',
+        '/>\n',
+        '    <Param name="scaleCenter" ',
+        'value="%d" ' % center_pt_num,
+        'description="Center of scaling:" ',
+        'enum_val0="origin" ',
+        'enum_val1="barycenter" ',
+        'enum_val2="custom point" ',
+        'enum_cardinality="3" ',
+        'type="RichEnum" ',
+        '/>\n',
+        '    <Param name="customCenter" ',
+        'x="%s" ' % custom_center_pt[0],
+        'y="%s" ' % custom_center_pt[1],
+        'z="%s" ' % custom_center_pt[2],
+        'description="Custom center" ',
+        'type="RichPoint3f" ',
+        '/>\n',
+        '    <Param name="unitFlag" ',
+        'value="%s" ' % str(unit).lower(),
+        'description="Scale to Unit bbox" ',
+        'type="RichBool" ',
+        '/>\n',
+        '    <Param name="Freeze" ',
+        'value="%s" ' % str(freeze).lower(),
+        'description="Freeze Matrix." ',
+        'type="RichBool" ',
+        '/>\n',
+        '    <Param name="ToAll" ',
+        'value="%s" ' % str(all_layers).lower(),
+        'description="Apply to all layers." ',
+        'type="RichBool" ',
+        '/>\n',
+        '  </filter>\n'])
+    write_filter(script, filter_xml)
+    return None
+
+
+
+
+# meshlabserver_path = 'C:\\Program Files\\VCG\\MeshLab'
+# os.environ['PATH'] = meshlabserver_path + os.pathsep + os.environ['PATH']
+
+meshlabserver_path = '/Applications/meshlab.app/Contents/MacOS'
 os.environ['PATH'] = meshlabserver_path + os.pathsep + os.environ['PATH']
 
 def has_hidden_attribute(filepath):
@@ -27,27 +165,23 @@ for filename in os.listdir(db_path):
         name = os.path.splitext(filename)[0]
         normalized_path=os.path.join(curr_directory, "normalized")
 
-        #aabb = mlx.files.measure_aabb(os.path.join(normalized_path, name + ".ply"))
-        #just to check that the files are centered
-
-        aabb = mlx.files.measure_aabb(mesh_path)
         simplified_mesh_path = os.path.join(curr_directory, "simplified", name + ".ply")
 
-        script = mlx.FilterScript(file_in=simplified_mesh_path,
-                                  file_out=os.path.join("normalized", name + ".ply"),
+        # #scale
+        scaling_script = mlx.FilterScript(file_in=simplified_mesh_path,
+                                  file_out=os.path.join("scaled", name + ".ply"),
                                   ml_version='2016.12')
 
 
-        #scale
-
-        # mlx.transform.scale(script,np.divide(1,aabb['size']))
-        # script.run_script()
-
+        scale_fixed(script=scaling_script,unit=True,center_pt='')
+        scaling_script.run_script()
 
         #center
-        script = mlx.FilterScript(file_in=os.path.join("normalized", name + ".ply"),
+        centering_script = mlx.FilterScript(file_in=os.path.join("scaled", name + ".ply"),
                                   file_out=os.path.join("normalized", name + ".ply"),
                                   ml_version='2016.12')
 
-        mlx.transform.translate(script , np.negative(aabb['center']))
-        script.run_script()
+        aabb = mlx.files.measure_aabb(os.path.join("scaled", name + ".ply"))
+        mlx.transform.translate(centering_script, np.negative(aabb['center']))
+        centering_script.run_script()
+
