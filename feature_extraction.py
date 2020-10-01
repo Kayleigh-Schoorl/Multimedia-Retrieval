c01aa7a9677f2839087ce2297c49e49628638c31
Kayleigh Schoorl <kayleighschoorl@hotmail.com>
2020-10-01 20:44:06 +0200

    First few features

--- feature_extraction.py
+++ feature_extraction.py
@@ -2,56 +2,40 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 curr_directory = os.getcwd()
-total_error=[]
 db_path = os.path.join(curr_directory, "images", "bw")
-count=[]
-i=0
 
-for filename in os.listdir(db_path):
+for folder in os.listdir(db_path):
+    if folder.startswith("."):
+        continue
 
-    extension = os.path.splitext(filename)[1]
-    mesh_name = os.path.splitext(filename)[0]
+    for filename in os.listdir(os.path.join(db_path, folder)):
+        if "Vase" not in filename:
+            continue
+        print(filename)
 
-    image_original = cv2.imread(os.path.join(db_path, filename),0)
-    image = cv2.bitwise_not(image_original)
+        # Read image
+        image_original = cv2.imread(os.path.join(db_path, folder, filename),0)
+        image = cv2.bitwise_not(image_original)
 
-    area_2 = cv2.countNonZero(image)
+        # Get area 
+        area = cv2.countNonZero(image)
+        print("Area: " + str(area))
 
+        # Get perimeter (only outer perimeter)
+        contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        perimeter = int(cv2.arcLength(contours[0],True))
+        print("Perimeter: " + str(perimeter))
 
-    contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # Compute compactness
+        area_2 = cv2.contourArea(contours[0])
+        compactness = (perimeter ** 2) / (4 * math.pi * area_2)
+        print("Compactness: " + str(compactness))
 
-    #for making image 3 channeled, optional
-    disp = cv2.merge((image, image, image))
+        # Compute circularity
+        circularity = 1 / compactness
+        print("Circularity: " + str(circularity))
 
-    disp = cv2.drawContours(disp, contours, -1, hierarchy=hierarchy, color=(0, 255, 0),thickness=5)
-
-
-
-    cnt = contours[0]
-    area_1 = cv2.contourArea(cnt)
-
-
-
-    #area
-    total_rows , total_cols = image.shape
-    #countnonzero = white pixels
-
-    error=abs(area_2-area_1)
-
-    print(str(area_1) + "      " +str(area_2) + "        "+ str(i) + "       "+ str(error))
-
-
-    cv2.imshow("disp",disp)
-    cv2.waitKey(0)
-
-    total_error.append(error)
-    count.append(i)
-    i+=1
-
-plt.plot(count, total_error)
-plt.title('error')
-plt.xlabel('cnt')
-plt.ylabel('error')
-plt.show()
+        print("\n")
