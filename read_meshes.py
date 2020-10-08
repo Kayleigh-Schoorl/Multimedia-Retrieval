516194e9fe26766e6ca05e692d96295fe4b3f041
Kayleigh Schoorl <kayleighschoorl@hotmail.com>
2020-10-08 19:18:35 +0200

    Add optional trimesh option to read_meshes

--- read_meshes.py
+++ read_meshes.py
@@ -1,13 +1,14 @@
 import numpy as np
 import trimesh
 import argparse
-import meshpy
 import pyvista as pv
 import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-M", "--mesh", help="input mesh")
 parser.add_argument("-S", "--smooth", help="show smooth mesh", action="store_true")
+parser.add_argument("-T", "--trimesh", help="use trimesh viewer", action="store_true")
+parser.add_argument("-E", "--edges", help="show edges", action="store_true")
 
 args = parser.parse_args()
 
@@ -16,8 +17,14 @@ if args.mesh:
     extension = splitted[len(splitted)-1]
 
     if extension == ".ply" or extension == ".off":
-        mesh = pv.read(args.mesh)
-        mesh.plot(show_edges=True, color="pink", window_size=[2048, 1536], background="white")
+
+        if args.trimesh:
+            mesh = trimesh.load(args.mesh)
+            mesh.show(smooth=args.smooth, flags={"wireframe":args.edges})
+
+        else:
+            mesh = pv.read(args.mesh)
+            mesh.plot(show_edges=args.edges, color="pink", window_size=[2048, 1536], background="white", smooth_shading=args.smooth)
 
     else:
         print("Please input a .off or .ply file.")
