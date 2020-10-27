3ceae45a1ccb55bba3b01e340cd55adb665754dc
Kayleigh Schoorl <kayleighschoorl@hotmail.com>
2020-10-27 23:40:36 +0100

    Add weights to ANN model

--- create_ann_model.py
+++ create_ann_model.py
@@ -10,6 +10,24 @@ import pickle
 
 @numba.jit(nopython=True)
 def distance_computation(v1, v2):
+
+    features = ["area",
+            "perimeter",
+            "compactness",
+            "circularity",
+            "centroid_x",
+            "centroid_y",
+            "bounding_box_x",
+            "bounding_box_y",
+            "bounding_box_w",
+            "bounding_box_h",
+            "rectangularity",
+            "diameter",
+            "eccentricity",
+            "skeleton_length"]
+
+    weights = [0.2,0.01,0.01,0.01,0.01,0.01,0.01,1,0.8,0.01,0.05,0.5,0.4,1]
+
     no_features = 14
     v1_images = [v1[i:i + no_features] for i in range(0, len(v1), no_features)]
     v2_images = [v2[i:i + no_features] for i in range(0, len(v2), no_features)]
@@ -20,7 +38,7 @@ def distance_computation(v1, v2):
         for image in v2_images:
             distance = 0
             for i in range(no_features):
-                distance += (query_image[i] - image[i])**2
+                distance += float(weights[i]) * (query_image[i] - image[i])**2
             distance = np.sqrt(distance)
             if distance < min_distance:
                 min_distance = distance
